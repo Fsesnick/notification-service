c17293157debd80c0ccfe8092b77def4b3d1e411
sesnick <fernanda.sesnick@vtex.com.br>
2022-12-14 10:08:25 -0300

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) { }
+
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+
+    //const {recipientId, category,content} = body
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Nova solicitacao de amizade',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
+  }
+}
