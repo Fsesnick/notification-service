f7f793e3559be5d86e82bdd7ac1f22d9e7112a48
sesnick <fernanda.sesnick@vtex.com.br>
2022-12-16 09:31:00 -0300

    primeiro teste content

---
+++ src/application/entities/content.spec.ts
@@ -0,0 +1,19 @@
+import { Content } from "./content"
+
+test('it should be able to create a notification content', () => {
+    const content = new Content(' Voce recebeu uma solicitaco de amizade');
+
+    expect(content).toBeTruthy();
+});
+
+test('it should not be able to create a notification content with less than 5 characters', () => {
+
+    expect(() => new Content('aa')).toThrow();
+});
+
+test('it should not be able to create a notification content with more than 240 characters', () => {
+
+    expect(() => new Content('a'.repeat(241))).toThrow();
+});
+
+//run  npm run test
